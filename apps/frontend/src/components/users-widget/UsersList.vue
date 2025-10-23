0da0370dd471ee58ea01c4ec8b6559ba9b87526d
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-23 22:24:28 +0300

    Implemented: logic at component UsersList

--- apps/frontend/src/components/users-widget/UsersList.vue
+++ apps/frontend/src/components/users-widget/UsersList.vue
@@ -5,11 +5,28 @@ export default {
   components: { UsersItem },
 
   props: ['users'],
+
+  emits: ['users-updated'],
+
+  methods: {
+    handleEdit(editedUser) {
+      this.$emit(
+        'users-updated',
+        this.users.map(u => (u.id === editedUser.id ? editedUser : u))
+      )
+    },
+  },
 }
 </script>
 
 <template>
-  <tbody id="elTBodyUsers">
-    <UsersItem v-for="user of users" :key="user.id" :data-id="user.id" :user />
+  <tbody>
+    <UsersItem
+      v-for="user of users"
+      :key="user.id"
+      :data-id="user.id"
+      :user
+      @user-edited="handleEdit"
+    />
   </tbody>
 </template>
