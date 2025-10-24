2d8e35bdc5216d912629919cf03a66537dd25077
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-24 08:12:55 +0300

    Fixed: rest cors

--- apps/backend/src/appRest.ts
+++ apps/backend/src/appRest.ts
@@ -1,12 +1,18 @@
+import cookieParser from 'cookie-parser'
 import express from 'express'
+import cors from 'cors'
 import routes from './routes.js'
 
 const appRest = express()
+const corsOptions = { origin: true, credentials: true }
+const urlencodedOptions = { extended: true }
 
-appRest.use(express.json())
-appRest.use(express.urlencoded({ extended: true }))
-appRest.use(routes)
-
-appRest.use(routes)
+appRest.use([
+  cookieParser() as any,
+  cors(corsOptions),
+  express.json(),
+  express.urlencoded(urlencodedOptions),
+  routes,
+])
 
 export default appRest
