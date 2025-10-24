53ff2512c99eb6fbc514cd6ced4f90c194b7548d
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-24 19:05:04 +0300

    Added: emits refresh at UsersUpdater

--- apps/frontend/src/components/users-widget/UsersUpdater.vue
+++ apps/frontend/src/components/users-widget/UsersUpdater.vue
@@ -1,12 +1,18 @@
 <script>
-export default {}
+export default {
+  emits: ['refresh'],
+}
 </script>
 
 <template>
   <header class="main-header">
     <h1>Панель Управления Пользователями ✨</h1>
     <div class="table-controls">
-      <button id="elButtonRefreshAll" class="control-btn refresh">
+      <button
+        id="elButtonRefreshAll"
+        class="control-btn refresh"
+        @click="$emit('refresh')"
+      >
         <span class="icon">🔄</span> Обновить данные
       </button>
     </div>
