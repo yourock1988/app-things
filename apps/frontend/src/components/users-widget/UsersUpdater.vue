10ca739ceb4101d0677f5879539f86aa112e110e
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-24 05:27:33 +0300

    Chore: component UsersUpdater.vue

--- apps/frontend/src/components/users-widget/UsersUpdater.vue
+++ apps/frontend/src/components/users-widget/UsersUpdater.vue
@@ -6,11 +6,7 @@ export default {}
   <header class="main-header">
     <h1>Панель Управления Пользователями ✨</h1>
     <div class="table-controls">
-      <button
-        @click="handleRefresh"
-        id="elButtonRefreshAll"
-        class="control-btn refresh"
-      >
+      <button id="elButtonRefreshAll" class="control-btn refresh">
         <span class="icon">🔄</span> Обновить данные
       </button>
     </div>
