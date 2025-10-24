0b3b970529e497cc4f6f8738a229b723c2c095a7
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-24 06:30:19 +0300

    Added: user to fixtures

--- apps/backend/src/utils/tables/usersTable.ts
+++ apps/backend/src/utils/tables/usersTable.ts
@@ -8,6 +8,13 @@ const usersTable: TUserRecord[] = [
     email: 'yourock88@super-mail.com',
     money: 42,
   },
+  {
+    id: '112',
+    nickname: 'youri',
+    password: 'qwe321',
+    email: 'youri@mail.com',
+    money: 33,
+  },
 ]
 
 export default usersTable
