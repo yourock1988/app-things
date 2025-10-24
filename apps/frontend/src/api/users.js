4939ab1d8a8b680e065d2a3f9990fb06c176cbf1
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-24 08:13:49 +0300

    Implemented: frontend api users

---
+++ apps/frontend/src/api/users.js
@@ -0,0 +1,29 @@
+import sendRequest from './sendRequest'
+
+const { APP_ORIGIN } = process.env
+const url = `${APP_ORIGIN}/api/v0/users/`
+
+export async function getUsers() {
+  const response = await sendRequest(url)
+  return response
+}
+
+export async function getUserById(id) {
+  const response = await sendRequest(url + id)
+  return response
+}
+
+export async function postUser(user) {
+  const response = await sendRequest(url, 'POST', user)
+  return response
+}
+
+export async function patchUserById(id, user) {
+  const response = await sendRequest(url + id, 'PATCH', user)
+  return response
+}
+
+export async function deleteUserById(id) {
+  const response = await sendRequest(url + id, 'DELETE')
+  return response
+}
