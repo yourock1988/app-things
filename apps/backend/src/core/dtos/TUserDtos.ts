52d2ec13e3384ea3fca954a83152d4120044885c
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-26 05:14:02 +0200

    Refactored: UserDto types from zod schemas

--- apps/backend/src/core/dtos/TUserDtos.ts
+++ apps/backend/src/core/dtos/TUserDtos.ts
@@ -1,12 +1,20 @@
-export type TUserAddDto = {
-  readonly nickname: string
-  readonly password: string
-  readonly email: string
-}
+import { z } from 'zod'
 
-export type TUserUpdateDto = {
-  readonly id: number
-  readonly password: string
-  readonly email: string
-  readonly money: number
-}
+import { zUserAddDto, zUserUpdateDto } from '../schemas/zUserDtos'
+
+export type TUserAddDto = z.infer<typeof zUserAddDto>
+
+export type TUserUpdateDto = z.infer<typeof zUserUpdateDto>
+
+// export type TUserAddDto = {
+//   readonly nickname: string
+//   readonly password: string
+//   readonly email: string
+// }
+
+// export type TUserUpdateDto = {
+//   readonly id: number
+//   readonly password: string
+//   readonly email: string
+//   readonly money: number
+// }
