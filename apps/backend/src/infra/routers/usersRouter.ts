8f10ce6b2b76a4ee01b0d062481b1cf2b2713265
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-26 08:28:19 +0200

    Added: middleware schema validator to userRouter

--- apps/backend/src/infra/routers/usersRouter.ts
+++ apps/backend/src/infra/routers/usersRouter.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 import { userControllerRest } from '../../utils/di/userControllerDi.js'
+import validateSchema from '../../utils/validateSchema.js'
+import { zUserAddDto, zUserUpdateDto } from '../../core/schemas/zUserDtos.js'
+import { TUserAddDto, TUserUpdateDto } from '../../core/dtos/TUserDtos.js'
 
 const usersRouter = Router()
 
+const addMiddleware = validateSchema<TUserAddDto>(zUserAddDto)
+const updateMiddleware = validateSchema<TUserUpdateDto>(zUserUpdateDto)
+
 usersRouter.get('/', userControllerRest.getAll)
 usersRouter.get('/:id', userControllerRest.getById)
-usersRouter.post('/', userControllerRest.add)
-usersRouter.patch('/:id', userControllerRest.updateById)
+usersRouter.post('/', addMiddleware, userControllerRest.add)
+usersRouter.patch('/:id', updateMiddleware, userControllerRest.updateById)
 usersRouter.delete('/:id', userControllerRest.removeById)
 
 export default usersRouter
