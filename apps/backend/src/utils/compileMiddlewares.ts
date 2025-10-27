bb03b2f6d52a2040590d5f8abda905f2a24a84e3
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-27 04:45:48 +0200

    Implemented: function compileMiddlewares

---
+++ apps/backend/src/utils/compileMiddlewares.ts
@@ -0,0 +1,7 @@
+export default function compileMiddlewares(middlewaresMap: any) {
+  return function ([event, ...args]: any[], next: any) {
+    global.console.log('hi, i am compiled middleware')
+    if (middlewaresMap[event]) middlewaresMap[event](args, next)
+    else next()
+  }
+}
