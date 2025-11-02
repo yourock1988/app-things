f1c9013b385ce8deb3154572cb05f4089c72d271
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-11-02 23:14:14 +0200

    Implemented: user ws api

---
+++ apps/frontend/src/api/ws/users.js
@@ -0,0 +1,33 @@
+import { io } from 'socket.io-client'
+
+const socket = io('ws://localhost:7700', { transports: ['websocket'] })
+
+socket.on('connect', () => global.console.log('connect'))
+socket.on('user:added', () => global.console.log('user:added'))
+
+export function getAll() {
+  return new Promise(resolve => socket.emit('user:getAll', '_', resolve))
+}
+
+export function getById(id) {
+  return new Promise(resolve => socket.emit('user:getById', id, resolve))
+}
+
+export function add(dto) {
+  return new Promise(resolve => socket.emit('user:add', dto, resolve))
+}
+
+export function updateById(id, dto) {
+  return new Promise(res => socket.emit('user:updateById', id, dto, res))
+}
+
+export function removeById(id) {
+  return new Promise((resolve, reject) =>
+    socket.emit('user:removeById', id, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  )
+}
+
+export default socket
