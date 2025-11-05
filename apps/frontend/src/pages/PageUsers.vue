d7b6cd700bd2d76a2ece6fddc0333cd7cb2a79cb
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-11-06 01:08:30 +0200

    Implemented: page users

---
+++ apps/frontend/src/pages/PageUsers.vue
@@ -0,0 +1,14 @@
+<script>
+import UsersWidget from '../components/users-widget/UsersWidget.vue'
+
+export default {
+  components: { UsersWidget },
+}
+</script>
+
+<template>
+  <div>
+    <h3>PAGE USERS</h3>
+    <UsersWidget />
+  </div>
+</template>
