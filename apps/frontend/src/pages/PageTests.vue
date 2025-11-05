434890c50b307be48c7df4c5b5b0663a159020c9
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-11-06 01:08:09 +0200

    Implemented: page tests

---
+++ apps/frontend/src/pages/PageTests.vue
@@ -0,0 +1,62 @@
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('users', ['err']),
+  },
+
+  methods: {
+    ...mapActions('users', [
+      'createUser',
+      'deleteUserById',
+      'updateUserById',
+      'readUserById',
+    ]),
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h3>PAGE TESTS</h3>
+
+    <div>{{ err }}</div>
+
+    <button @click="deleteUserById(42)">WTF DELETE</button>
+
+    <button
+      @click="updateUserById({ id: 111, user: { money: 0, password: 'xxxx' } })"
+    >
+      WTF UPDATE 0
+    </button>
+
+    <button @click="updateUserById({ id: 42, user: {} })">WTF UPDATE 1</button>
+
+    <button
+      @click="updateUserById({ id: 42, user: { money: 0, password: 'xxxx' } })"
+    >
+      WTF UPDATE 2
+    </button>
+
+    <button
+      @click="updateUserById({ id: 42, user: { money: 0, password: 'xxxxx' } })"
+    >
+      WTF UPDATE 3
+    </button>
+
+    <button
+      @click="
+        createUser({
+          nickname: 'nicknameSchema',
+          password: 'passwordSchema',
+          email: 'emailSchema',
+        })
+      "
+    >
+      WTF CREATE
+    </button>
+
+    <button @click="readUserById(42)">WTF GET</button>
+  </div>
+</template>
