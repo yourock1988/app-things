97eb2df0f7fbd968f0a6d480b7366a8d8525ae01
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-11-30 12:14:29 +0200

    Extracted: component SignUpForm

---
+++ apps/frontend/src/components/SignUpForm.vue
@@ -0,0 +1,87 @@
+<script>
+// import { mapActions, mapState } from 'vuex'
+import FormField from '@/components/FormField.vue'
+
+export default {
+  components: { FormField },
+
+  data() {
+    return {
+      isLoading: false,
+      items: ['California', 'Florida', 'Texas'],
+      dto: this.initDto(),
+      err: null,
+      // err: {
+      //   _errors: ['very bad'],
+      //   isConfirmed: {
+      //     _errors: ['bad'],
+      //   },
+      //   city: {
+      //     _errors: ['bad'],
+      //   },
+      // },
+    }
+  },
+
+  computed: {
+    // ...mapState('auth', ['err']),
+  },
+
+  methods: {
+    // ...mapActions('auth', ['signUp']),
+
+    initDto() {
+      return {
+        isAgree: false,
+        nickname: '',
+        password: '',
+        repasswd: '',
+        email: '',
+        phone: '',
+        city: '',
+      }
+    },
+
+    async handleSubmit() {
+      this.isLoading = true
+      // await this.signUp({ ...this.dto })
+      this.dto = this.initDto()
+      this.isLoading = false
+    },
+  },
+}
+</script>
+
+<template>
+  <v-form @submit.prevent="handleSubmit">
+    <v-row class="justify-center">
+      <v-col cols="12">
+        <FormField v-model="dto" :err field="nickname" />
+      </v-col>
+      <v-col cols="12">
+        <FormField v-model="dto" :err field="password" />
+      </v-col>
+      <v-col cols="12">
+        <FormField v-model="dto" :err field="repasswd" />
+      </v-col>
+      <v-col cols="6">
+        <FormField v-model="dto" :err field="email" />
+      </v-col>
+      <v-col cols="6">
+        <FormField v-model="dto" :err field="phone" />
+      </v-col>
+      <v-col cols="12">
+        <FormField v-model="dto" :err field="city" comp="v-select" :items />
+      </v-col>
+      <v-col cols="12">
+        <FormField v-model="dto" :err field="isAgree" comp="v-checkbox" />
+      </v-col>
+      <v-col cols="12">
+        <v-btn :is-loading type="submit">Submit</v-btn>
+      </v-col>
+      <v-col v-if="err?._errors?.length > 0" cols="12">
+        <p>{{ err?._errors }}</p>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
--- apps/frontend/src/pages/PageSignUp.vue
+++ apps/frontend/src/pages/PageSignUp.vue
@@ -1,54 +1,8 @@
 <script>
-import FormField from '@/components/FormField.vue'
-// import { mapActions, mapState } from 'vuex'
+import SignUpForm from '../components/SignUpForm.vue'
 
 export default {
-  components: { FormField },
-
-  data() {
-    return {
-      isLoading: false,
-      items: ['California', 'Florida', 'Texas'],
-      dto: this.initDto(),
-      err: null,
-      // err: {
-      //   _errors: ['very bad'],
-      //   isConfirmed: {
-      //     _errors: ['bad'],
-      //   },
-      //   city: {
-      //     _errors: ['bad'],
-      //   },
-      // },
-    }
-  },
-
-  computed: {
-    // ...mapState('auth', ['err']),
-  },
-
-  methods: {
-    // ...mapActions('auth', ['signUp']),
-
-    initDto() {
-      return {
-        isConfirmed: false,
-        nickname: '',
-        password: '',
-        repasswd: '',
-        email: '',
-        phone: '',
-        city: '',
-      }
-    },
-
-    async handleSubmit() {
-      this.isLoading = true
-      // await this.signUp({ ...this.dto })
-      this.dto = this.initDto()
-      this.isLoading = false
-    },
-  },
+  components: { SignUpForm },
 }
 </script>
 
@@ -57,58 +11,8 @@ export default {
     <v-col cols="12" sm="8" md="6" xl="4">
       <v-sheet border="thin" rounded="xl">
         <v-container>
-          <h5 class="text-h5 text-center">SIGN UP</h5>
-          <v-form @submit.prevent="handleSubmit">
-            <v-row class="justify-center">
-              <v-col cols="12">
-                <FormField v-model="dto" :err field="nickname" />
-              </v-col>
-              <v-col cols="12">
-                <FormField v-model="dto" :err field="password" />
-              </v-col>
-              <v-col cols="12">
-                <FormField v-model="dto" :err field="repasswd" />
-              </v-col>
-              <v-col cols="12">
-                <FormField v-model="dto" :err field="email" />
-              </v-col>
-              <v-col cols="12">
-                <FormField v-model="dto" :err field="phone" />
-              </v-col>
-              <v-col cols="12">
-                <FormField
-                  v-model="dto"
-                  :err
-                  field="city"
-                  comp="v-select"
-                  :items
-                />
-              </v-col>
-
-              <v-col cols="12">
-                <FormField
-                  v-model="dto"
-                  :err
-                  field="isConfirmed"
-                  comp="v-checkbox"
-                />
-              </v-col>
-
-              <v-col cols="12">
-                <v-btn
-                  :is-loading
-                  size="x-large"
-                  type="submit"
-                  text="sign up"
-                  block
-                ></v-btn>
-              </v-col>
-
-              <v-col v-if="err?._errors?.length > 0" cols="12">
-                <p>{{ err?._errors }}</p>
-              </v-col>
-            </v-row>
-          </v-form>
+          <h5 class="text-h5 text-center">SIGNUP</h5>
+          <SignUpForm />
         </v-container>
       </v-sheet>
     </v-col>
