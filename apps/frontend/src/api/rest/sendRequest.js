84535c5bcbb8562b85cdead7cbd05dc21347537e
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-12-03 15:13:46 +0200

    Implemented: function sendRequest

---
+++ apps/frontend/src/api/rest/sendRequest.js
@@ -0,0 +1,41 @@
+import {
+  FetchRequestError,
+  HttpStatusError,
+  ParseJsonError,
+} from '../../errors.js'
+
+export default async function sendRequest(url, method = 'GET', payload = null) {
+  let resp
+  let data
+  let text
+  const options = { method, signal: AbortSignal.timeout(5000) }
+
+  if (payload) {
+    options.headers = { 'Content-Type': 'application/json' }
+    options.body = JSON.stringify(payload)
+  }
+
+  try {
+    resp = await fetch(url, options)
+  } catch (e) {
+    if (e instanceof TypeError) return [new FetchRequestError(e.message)]
+    if (e.name === 'AbortError') return [new FetchRequestError(e.message)]
+    throw new Error(e.message)
+  }
+
+  if (resp.status === 204) return [null, true]
+
+  try {
+    text = await resp.text()
+    data = JSON.parse(text)
+  } catch (e) {
+    if (e instanceof SyntaxError) return [new ParseJsonError(e.message), text]
+    throw new Error(e.message)
+  }
+
+  if ([400, 401, 403, 404, 405, 500].includes(resp.status)) {
+    return [new HttpStatusError(data)]
+  }
+
+  return [null, data]
+}
