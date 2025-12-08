cb6607e6583d6d042095e93610327bc75b158385
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-12-08 14:39:52 +0200

    Extracted: data object messages.json from phoneSchema.ts

--- apps/backend/src/core/schemas/phoneSchema.ts
+++ apps/backend/src/core/schemas/phoneSchema.ts
@@ -1,10 +1,8 @@
 import z from 'zod'
 import validatePhoneStrict from '../../utils/validatePhoneStrict.js'
+import messages from './messages.json' with { type: 'json' }
 
-const msg = {
-  require: 'Пришлите это поле',
-  corrtel: 'Введите корректный номер телефона',
-}
+const msg = { ...messages, ...messages.phone }
 
 export default z
   .string({ required_error: msg.require })
