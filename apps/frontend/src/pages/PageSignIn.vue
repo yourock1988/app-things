f7385a6b84fd9727e3f2a29c9bf508a68120ac53
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-12-17 19:03:29 +0200

    fe: Refactored component PageSignIn.vue

--- apps/frontend/src/pages/PageSignIn.vue
+++ apps/frontend/src/pages/PageSignIn.vue
@@ -1,22 +1,38 @@
 <script>
-import { mapState } from 'vuex/dist/vuex.cjs.js'
-import SignInForm from '@/components/SignInForm.vue'
+import { mapActions, mapState } from 'vuex/dist/vuex.cjs.js'
 import CardSuccess from '@/ui/CardSuccess.vue'
 import FormSheet from '@/ui/FormSheet.vue'
+import TurboForm from '@/ui/TurboForm.vue'
 
 export default {
-  components: { SignInForm, FormSheet, CardSuccess },
+  components: { TurboForm, FormSheet, CardSuccess },
 
   data() {
     return {
       sizing: { cols: 12, sm: 8, md: 6, xl: 4 },
       title: 'Вы вошли в систему!',
       text: 'Теперь можете юзать приложение под ником',
+      cols: 12,
+      fields: { nickname: null, password: null },
     }
   },
 
   computed: {
-    ...mapState('auth', ['session']),
+    ...mapState('auth', ['err', 'session']),
+  },
+
+  methods: {
+    ...mapActions('auth', ['signIn']),
+    initTest() {
+      return {
+        nickname: 'xxx',
+        password: 'xX1!xxxx',
+      }
+    },
+    async submit({ resolve, ...dto }) {
+      await this.signIn(dto)
+      resolve()
+    },
   },
 }
 </script>
@@ -26,6 +42,6 @@ export default {
     <CardSuccess v-if="session" :title :text link="/cars" ankhor="Юзать">
       <b>{{ session.nickname }}</b>
     </CardSuccess>
-    <SignInForm v-else />
+    <TurboForm v-else :fields :err :cols :init-test @submit="submit" />
   </FormSheet>
 </template>
