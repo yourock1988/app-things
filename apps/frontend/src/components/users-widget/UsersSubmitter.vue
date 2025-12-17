43e426f8b55845fe43aa1d0fddcef5512a3ba255
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-12-17 18:13:07 +0200

    fe: Refactored component UsersSubmitter.vue

--- apps/frontend/src/components/users-widget/UsersSubmitter.vue
+++ apps/frontend/src/components/users-widget/UsersSubmitter.vue
@@ -1,18 +1,44 @@
 <script>
-import FormSheet from '../../ui/FormSheet.vue'
-import UsersForm from '../UsersForm.vue'
+import { mapActions, mapState } from 'vuex'
+import FormSheet from '@/ui/FormSheet.vue'
+import TurboForm from '@/ui/TurboForm.vue'
 
 export default {
-  components: { UsersForm, FormSheet },
+  components: { TurboForm, FormSheet },
 
   data() {
     return {
       sizing: { cols: 12 },
+      cols: 3,
+      fields: {
+        nickname: null,
+        password: null,
+        email: null,
+      },
     }
+  },
+  computed: {
+    ...mapState('users', ['err']),
+  },
+  methods: {
+    ...mapActions('users', ['createUser']),
+    async submit({ resolve, ...dto }) {
+      await this.createUser(dto)
+      resolve()
+    },
+    initTest() {
+      return {
+        nickname: 'Foobar',
+        password: 'qwerty123',
+        email: 'foobar@mail.xxx',
+      }
+    },
   },
 }
 </script>
 
 <template>
-  <FormSheet caption="Create car" :sizing><UsersForm /></FormSheet>
+  <FormSheet caption="Create car" :sizing>
+    <TurboForm :fields :err :cols :init-test @submit="submit" />
+  </FormSheet>
 </template>
