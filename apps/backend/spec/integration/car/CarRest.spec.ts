dc7b4ee327a492f7eb223fc305860b28017b43c6
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2026-03-11 12:09:41 +0200

    be: Tested integretion GET /api/v0/cars/:id

--- apps/backend/spec/integration/car/CarRest.spec.ts
+++ apps/backend/spec/integration/car/CarRest.spec.ts
@@ -4,67 +4,72 @@ import carFixture from '../../fixtures/carFixture.js'
 import carsFixture from '../../fixtures/carsFixture.js'
 
 describe('GET /cars', () => {
-  it('should status code is 200', async () => {
+  let response
+
+  beforeAll(async () => {
     const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
+    response = await agent.get('/api/v0/cars')
+  })
+
+  it('should status code is 200', async () => {
     expect(response.status).toBe(200)
   })
-
   it('should headers has content-type json', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
     expect(response.headers['content-type']).toContain('application/json')
   })
-
   it('should headers has content-type utf-8', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
     expect(response.headers['content-type']).toContain('utf-8')
   })
-
-  it('should headers cors enabled', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
+  it('should cors enabled', async () => {
     expect(response.headers['access-control-allow-origin']).toContain('*')
   })
-
-  it('should headers cors allow credentials', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
+  it('should cors allow credentials', async () => {
     expect(response.headers['access-control-allow-credentials']).toBeTruthy()
   })
-
-  it('should headers cors allow credentials', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
+  it('should header x-powered-by removed', async () => {
     expect(response.headers['x-powered-by']).toBeUndefined()
   })
-
-  it('should headers cors allow credentials', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
+  it('should helmet middleware enabled', async () => {
     expect(response.headers['x-frame-options']).toBe('SAMEORIGIN')
   })
-
   it('should body is array', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
     expect(response.body).toBeInstanceOf(Array)
   })
-
   it('should body equals fixture', async () => {
-    const agent = supertest(appHttp)
-    const response = await agent.get('/api/v0/cars')
     expect(response.body).toEqual(carsFixture)
   })
 })
 
-// it('GET /cars/1001', async () => {
-//   const response = await supertest(appHttp)
-//     .get('/api/v0/cars/1001')
-//     .expect(200)
-//     .expect('Content-Type', /json/)
-//     .expect('Content-Type', /utf-8/)
+describe('GET /cars/1001', () => {
+  let response
 
-//   expect(response.body).toEqual(carFixture)
-// })
+  beforeAll(async () => {
+    const agent = supertest(appHttp)
+    response = await agent.get('/api/v0/cars/1001')
+  })
+
+  it('should status code is 200', async () => {
+    expect(response.status).toBe(200)
+  })
+  it('should headers has content-type json', async () => {
+    expect(response.headers['content-type']).toContain('application/json')
+  })
+  it('should headers has content-type utf-8', async () => {
+    expect(response.headers['content-type']).toContain('utf-8')
+  })
+  it('should cors enabled', async () => {
+    expect(response.headers['access-control-allow-origin']).toContain('*')
+  })
+  it('should cors allow credentials', async () => {
+    expect(response.headers['access-control-allow-credentials']).toBeTruthy()
+  })
+  it('should header x-powered-by removed', async () => {
+    expect(response.headers['x-powered-by']).toBeUndefined()
+  })
+  it('should helmet middleware enabled', async () => {
+    expect(response.headers['x-frame-options']).toBe('SAMEORIGIN')
+  })
+  it('should body equals fixture', async () => {
+    expect(response.body).toEqual(carFixture)
+  })
+})
